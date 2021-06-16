b734933b898b9ac847028230aab05917217afaa2
Holly Maria <72448486+hollymaria@users.noreply.github.com>
2021-06-16 17:19:55 +0100

    Create formatting_string_with_methods.rb

---
+++ formatting_string_with_methods.rb
@@ -0,0 +1,28 @@
+# Ruby-project
+
+print "What's your first name?"
+
+first_name = gets.chomp
+
+first_name.capitalize!
+
+print "What is your last name?"
+
+
+last_name = gets.chomp
+
+last_name.capitalize!
+
+print "What is your favourite animal?"
+
+animal = gets.chomp
+
+animal.capitalize!
+
+print "How many of them would you like?"
+
+many = gets.chomp
+
+puts "Your name is #{first_name} #{last_name}! Your favourite animal is a #{animal} and you only want #{many} of them"
+
+Fun lesson I learnt on codeacademy.
